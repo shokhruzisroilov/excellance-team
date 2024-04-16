80b52c34f42f82bd0d01b7efba8358de40f49fb5
shokhruzisroilov <shokhruzisroilov@gmail.com>
2024-04-16 14:22:07 +0500

    workData description

--- src/utils/worksData.js
+++ src/utils/worksData.js
@@ -1,31 +1,27 @@
 import onlineShop from '../assets/image/e-commerse-online-shop.webp'
 import zummaPharma from '../assets/image/zumma-pharma.webp'
 import exellanceTeam from '../assets/image/exellance-team.webp'
-import kasbiSchool from '../assets/image/kasbi-school.webp'
 
 export const worksData = [
 	{
 		id: 1,
 		image: onlineShop,
 		title: 'Dreams Shop',
-		description: `Salom,
-Biz sizning biz bilan hamkorlik qilishni istaydigan o'zaro qo'ng'iroq qilmoqchimiz. Sizning erlik faoliyatingizga foydalanuvchilarga o'z xizmatlaringizni taqdim etish imkoniyatini beradigan chiroyli veb-sayt yaratishga tayyorlikda bo'lamiz.`,
+		description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dicta commodi assumenda cum voluptates delectus beatae sint obcaecati eaque eos dolore ad voluptatum id, doloremque aliquam eveniet sed explicabo officiis?`,
 		link: 'https://orginal-uz-e-commerse.vercel.app/',
 	},
 	{
 		id: 2,
 		image: zummaPharma,
 		title: 'Zumma Dorixona websayti',
-		description: `Salom,
-Biz sizning biz bilan hamkorlik qilishni istaydigan o'zaro qo'ng'iroq qilmoqchimiz. Sizning erlik faoliyatingizga foydalanuvchilarga o'z xizmatlaringizni taqdim etish imkoniyatini beradigan chiroyli veb-sayt yaratishga tayyorlikda bo'lamiz.`,
+		description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dicta commodi assumenda cum voluptates delectus beatae sint obcaecati eaque eos dolore ad voluptatum id, doloremque aliquam eveniet sed explicabo officiis?`,
 		link: 'https://zumma-pharma-site.netlify.app/',
 	},
 	{
 		id: 3,
 		image: exellanceTeam,
 		title: 'ExellanceTeam websayti',
-		description: `Salom,
-Biz sizning biz bilan hamkorlik qilishni istaydigan o'zaro qo'ng'iroq qilmoqchimiz. Sizning erlik faoliyatingizga foydalanuvchilarga o'z xizmatlaringizni taqdim etish imkoniyatini beradigan chiroyli veb-sayt yaratishga tayyorlikda bo'lamiz.`,
+		description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dicta commodi assumenda cum voluptates delectus beatae sint obcaecati eaque eos dolore ad voluptatum id, doloremque aliquam eveniet sed explicabo officiis?`,
 		link: 'https://t.me/excellance',
 	},
 ]
